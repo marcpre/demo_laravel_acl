d23dfbbfd5b5ff0ed75e2eda7b9bd93b8970c209
marcpre <marcus.presich@gmail.com>
2017-12-03 10:51:57 +0100

    setup  roles & users database with seeds

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Role;
 
 class UsersTableSeeder extends Seeder
 {
@@ -11,16 +13,30 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin@admin.com',
-            'password' => bcrypt('admin'),
-        ]);
+        $role_user = Role::where('name', 'User')->first();
+        $role_author = Role::where('name', 'Author')->first();
+        $role_admin = Role::where('name', 'Admin')->first();
+               
+        $user = new User();
+        $user->name = 'user';
+        $user->email = 'user@user.com';
+        $user->password = bcrypt('user');
+        $user->save();
+        $user->roles()->attach($role_user);
         
-        DB::table('users')->insert([
-            'name' => 'user',
-            'email' => 'user@user.com',
-            'password' => bcrypt('user'),
-        ]);
+        $author = new User();
+        $author->name = 'author';
+        $author->email = 'author@author.com';
+        $author->password = bcrypt('author');
+        $author->save();
+        $author->roles()->attach($role_author);
+        
+        $admin = new User();
+        $admin->name = 'admin';
+        $admin->email = 'admin@admin.com';
+        $admin->password = bcrypt('admin');
+        $admin->save();
+        $admin->roles()->attach($role_admin);
+        
     }
 }
